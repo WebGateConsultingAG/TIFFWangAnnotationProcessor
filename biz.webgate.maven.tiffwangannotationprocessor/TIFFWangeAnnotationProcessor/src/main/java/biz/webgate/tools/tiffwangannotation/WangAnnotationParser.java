c4a3437aaead36efbf22d31dd37a65b186ecee94
Mirko Eberlein <mirko.eberlein@webgate.biz>
2015-09-14 14:14:05 +0200

    act changes

--- biz.webgate.maven.tiffwangannotationprocessor/TIFFWangeAnnotationProcessor/src/main/java/biz/webgate/tools/tiffwangannotation/WangAnnotationParser.java
+++ biz.webgate.maven.tiffwangannotationprocessor/TIFFWangeAnnotationProcessor/src/main/java/biz/webgate/tools/tiffwangannotation/WangAnnotationParser.java
@@ -2,6 +2,7 @@ package biz.webgate.tools.tiffwangannotation;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.ArrayList;
 
 import biz.webgate.tools.tiffwangannotation.annotations.Type5Annotation;
 
@@ -35,6 +36,32 @@ public enum WangAnnotationParser {
 		return container;
 	}
 
+	public byte[] writeannotation(WangAnnotationContainer container){
+		ArrayList<Byte> annoList = new ArrayList<Byte>();
+		byte[] header = ByteBuffer.allocate(4).putInt(container.getHeader()).array();
+		for(Byte b : header){
+			annoList.add(b);
+		}
+		byte[] win32 = ByteBuffer.allocate(4).putInt(container.isWin32()?1:0).array();
+		for(Byte b : win32){
+			annoList.add(b);
+		}
+		
+		for(IAnnotation ia : container.getAnnotations()){
+			Byte[] anoBytes = ia.serialize();
+			for(Byte b : anoBytes){
+				annoList.add(b);
+			}
+		}
+		byte[] retBytes =  new byte[annoList.size()];
+		int i = 0;
+		for(byte b : annoList){
+			retBytes[i] = b;
+			i++;
+		}
+		return retBytes;
+	}
+	
 	private IAnnotation buildAnnoation(ByteBuffer buffer, int blockType, int blockSize) {
 		
 		switch (blockType) {
--- biz.webgate.maven.tiffwangannotationprocessor/TIFFWangeAnnotationProcessor/src/main/java/biz/webgate/tools/tiffwangannotation/annotations/helpers/Point.java
+++ biz.webgate.maven.tiffwangannotationprocessor/TIFFWangeAnnotationProcessor/src/main/java/biz/webgate/tools/tiffwangannotation/annotations/helpers/Point.java
@@ -26,6 +26,7 @@ public class Point {
 	public int getY() {
 		return y;
 	}
-
+	
+	
 	
 }
